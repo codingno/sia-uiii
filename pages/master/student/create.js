b783009e4ec3bbff003cf0e9e3049bbee7ad537e
codingno <codingno@gmail.com>
2021-12-27 01:21:36 +0700

    first commit student create client

---
+++ pages/master/student/create.js
@@ -0,0 +1,325 @@
+import { useState, useEffect } from "react";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import Stack from "@mui/material/Stack";
+
+import FormContainer from "../../../components/utils/FormContainer";
+import FormLayout from "../../../components/utils/FormLayout";
+import FormParent from "../../../components/utils/FormParent";
+
+import axios from "axios";
+import { useRouter } from "next/router";
+
+export default function () {
+	const router = useRouter()
+
+	const entryStatusOptions = [
+		{
+			id : 1,
+			name : 'NEW',
+		},
+		{
+			id : 2,
+			name : 'TRANSFER',
+		},
+	]
+	const entrySemesterOptions = [
+		{
+			id : 1,
+			name : 1,
+		},
+		{
+			id : 2,
+			name : 2,
+		},
+	]
+	const genderOptions = [
+		{
+			id : 1,
+			name : 'MAN',
+		},
+		{
+			id : 2,
+			name : 'WOMAN',
+		},
+	]
+	const [identityTypeOptions, setIdentityTypeOptions] = useState([])
+	const [departementOptions, setDepartementOptions] = useState([])
+	const [teacherOptions, setTeacherOptions] = useState([])
+	const [statusOptions, setStatusOptions] = useState([])
+
+  const [first_name, setFirstName] = useState("");
+  const [last_name, setLastName] = useState("");
+  const [middle_name, setMiddleName] = useState("");
+  const [place_of_birth, setPlaceOfBirth] = useState("");
+  const [date_of_birth, setDateOfBirth] = useState("");
+  const [gender, setGender] = useState(genderOptions[0].id);
+  const [identity_id, setIdentityID] = useState("");
+  const [identity_type_id, setIdentityType] = useState("");
+
+  const [user_id, setUserID] = useState("");
+  const [student_number, setStudentNumber] = useState("");
+  const [teacher_id, setTeacherID] = useState("");
+  const [entry_year, setEntryYear] = useState("");
+  const [entry_semester, setEntrySemester] = useState(entrySemesterOptions[0].id);
+  const [entry_status, setEntryStatus] = useState(entryStatusOptions[0].id);
+  const [departement_id, setDepartement] = useState("");
+  const [status, setStatus] = useState("");
+
+	useEffect(() => {
+			getTeacher()
+	},[departement_id])
+
+	async function getTeacher() {
+		try {
+			const { data } = await axios.get('/api/teacher')
+			const teachers = data.data.filter(item => item.departement_id == departement_id)
+			setTeacherOptions(teachers)
+		} catch (error) {
+			if (error.response) {
+				if (error.response.status == 404) return;
+				alert(error.response.data);
+			}
+			alert(error);
+		}	
+	}
+
+	useEffect(() => {
+		if(departementOptions.length == 0)
+			getDepartement()
+	},[departementOptions])
+
+	async function getDepartement() {
+		try {
+			const { data } = await axios.get('/api/departement')
+			setDepartementOptions(data.data)
+		} catch (error) {
+			if (error.response) {
+				if (error.response.status == 404) return;
+				alert(error.response.data);
+			}
+			alert(error);
+		}	
+	}
+
+	useEffect(() => {
+		if(identityTypeOptions.length == 0)
+			getIdentityType()
+	},[identityTypeOptions])
+
+	async function getIdentityType() {
+		try {
+			const { data } = await axios.get('/api/identity-type')
+			setIdentityTypeOptions(data.data)
+		} catch (error) {
+			if (error.response) {
+				if (error.response.status == 404) return;
+				alert(error.response.data);
+			}
+			alert(error);
+		}	
+	}
+
+	useEffect(() => {
+		if(statusOptions.length == 0)
+			getStudentStatus()
+	},[statusOptions])
+
+	async function getStudentStatus() {
+		try {
+			const { data } = await axios.get('/api/student-status')
+			setStatusOptions(data.data)
+		} catch (error) {
+			if (error.response) {
+				if (error.response.status == 404) return;
+				alert(error.response.data);
+			}
+			alert(error);
+		}	
+	}
+
+	async function submitDepartement() {
+		try {
+			const sendData = {
+				name,
+				code,
+				label,
+				study_type_id : studyType,
+				faculty_id : faculty,
+				status,
+				course_credits : courseCredits,
+			}	
+			const { data } = await axios.post('/api/departement', sendData)
+			alert("Departement successfully created.")
+			router.back()
+		} catch (error) {
+			if(error.response) {
+        console.log(`🚀 ~ file: create.js ~ line 67 ~ submitDepartement ~ error.response`, error.response)
+				alert(error.response.data)
+			}	
+			alert(error)
+		}	
+	}
+
+  return (
+    <FormLayout title="Student Create | SIA UIII" titlePage="Student Create">
+      <Stack
+        mb={4}
+        sx={{
+          width: 640,
+        }}
+      >
+        <FormContainer
+          label="Student Number"
+          name="student_number"
+          value={student_number}
+          setValue={setStudentNumber}
+        />
+        <FormContainer
+          label="Entry Year"
+          name="entry_year"
+          value={entry_year}
+          setValue={setEntryYear}
+        />
+				<FormParent label="Entry Semester">
+					<Select
+						displayEmpty
+						value={entry_semester}
+						onChange={(e) => setEntrySemester(e.target.value)}
+						inputProps={{ "aria-label": "Without label" }}
+					>
+						{entrySemesterOptions.length > 0 && entrySemesterOptions.map(item => <MenuItem value={item.id}>{item.name}</MenuItem>)}
+					</Select>
+				</FormParent>
+				<FormParent label="Entry Status">
+					<Select
+						displayEmpty
+						value={entry_status}
+						onChange={(e) => setEntryStatus(e.target.value)}
+						inputProps={{ "aria-label": "Without label" }}
+					>
+						{entryStatusOptions.length > 0 && entryStatusOptions.map(item => <MenuItem value={item.id}>{item.name}</MenuItem>)}
+					</Select>
+				</FormParent>
+				<FormParent label="Departement">
+					<Select
+						displayEmpty
+						value={departement_id}
+						onChange={(e) => setDepartement(e.target.value)}
+						inputProps={{ "aria-label": "Without label" }}
+					>
+						<MenuItem value={""}>
+							<em>None</em>
+						</MenuItem>
+						{departementOptions.length > 0 && departementOptions.map(item => <MenuItem value={item.id}>{item.name}</MenuItem>)}
+					</Select>
+				</FormParent>
+				<FormParent label="Teacher Academic">
+					<Select
+						displayEmpty
+						value={teacher_id}
+						onChange={(e) => setTeacherID(e.target.value)}
+						inputProps={{ "aria-label": "Without label" }}
+					>
+						<MenuItem value={""}>
+							<em>None</em>
+						</MenuItem>
+						{teacherOptions.length > 0 && teacherOptions.map(item => <MenuItem value={item.id}>{item.name}</MenuItem>)}
+					</Select>
+				</FormParent>
+				<FormParent label="Status">
+					<Select
+						displayEmpty
+						value={status}
+						onChange={(e) => setStatus(e.target.value)}
+						inputProps={{ "aria-label": "Without label" }}
+					>
+						<MenuItem value={""}>
+							<em>None</em>
+						</MenuItem>
+						{statusOptions.length > 0 && statusOptions.map(item => <MenuItem value={item.value}>{item.label}</MenuItem>)}
+					</Select>
+				</FormParent>
+        <FormContainer
+          label="First Name"
+          name="first_name"
+          value={first_name}
+          setValue={setFirstName}
+        />
+        <FormContainer
+          label="Middle Name"
+          name="middle_name"
+          value={middle_name}
+          setValue={setMiddleName}
+        />
+        <FormContainer
+          label="Last Name"
+          name="last_name"
+          value={last_name}
+          setValue={setLastName}
+        />
+        <FormContainer
+          label="Place Of Birth"
+          name="place_of_birth"
+          value={place_of_birth}
+          setValue={setPlaceOfBirth}
+        />
+        <FormContainer
+          label="Date Of Birth"
+          name="date_of_birth"
+          value={date_of_birth}
+          setValue={setDateOfBirth}
+        />
+				<FormParent label="Gender">
+					<Select
+						displayEmpty
+						value={gender}
+						onChange={(e) => setGender(e.target.value)}
+						inputProps={{ "aria-label": "Without label" }}
+					>
+						{genderOptions.length > 0 && genderOptions.map(item => <MenuItem value={item.id}>{item.name}</MenuItem>)}
+					</Select>
+				</FormParent>
+        <FormContainer
+          label="Identity ID"
+          name="identity_id"
+          value={identity_id}
+          setValue={setIdentityID}
+        />
+				<FormParent label="Identity Type">
+					<Select
+						displayEmpty
+						value={identity_type_id}
+						onChange={(e) => setIdentityType(e.target.value)}
+						inputProps={{ "aria-label": "Without label" }}
+					>
+						<MenuItem value={""}>
+							<em>None</em>
+						</MenuItem>
+						{identityTypeOptions.length > 0 && identityTypeOptions.map(item => <MenuItem value={item.id}>{item.name}</MenuItem>)}
+					</Select>
+				</FormParent>
+        <Stack
+          direction="row"
+          alignItems="center"
+          ml={5}
+          mt={3}
+          sx={{ width: "60%", display: "flex", justifyContent: "flex-start" }}
+        >
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{
+              width: 150,
+            }}
+            startIcon={() => <></>}
+						onClick={submitDepartement}
+          >
+            Submit
+          </Button>
+        </Stack>
+      </Stack>
+    </FormLayout>
+  );
+}
