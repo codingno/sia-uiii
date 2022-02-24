18b48d6966b731aac62d9efab4d8247daa53b029
codingno <codingno@gmail.com>
2022-02-25 00:59:54 +0700

    add image server

---
+++ server/index.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static(path.join(__dirname, '../public/uploads')))
+
+app.listen(process.env.IMAGE_SERVER_PORT, () => console.log("Run Image Server on PORT:" + process.env.IMAGE_SERVER_PORT))
